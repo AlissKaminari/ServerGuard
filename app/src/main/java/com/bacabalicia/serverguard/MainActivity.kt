6c5e1898128b6d8ff099acbb15407c32683ac518
Alicia Jazmin Bacab Chi <aliciabacab95@gmail.com>
2026-03-25 17:58:43 -0600

    Interfaz del login pantalla

--- app/src/main/java/com/bacabalicia/serverguard/MainActivity.kt
+++ app/src/main/java/com/bacabalicia/serverguard/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.bacabalicia.serverguard
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Referencias a los elementos del XML
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val tvRegister = findViewById<TextView>(R.id.tvRegister)
+
+        // Evento del botón LOGIN
+        btnLogin.setOnClickListener {
+
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            // Validación
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                // Simulación de login exitoso
+                Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+
+                // val intent = Intent(this, DashboardActivity::class.java)
+                // startActivity(intent)
+            }
+        }
+
+        // Evento de REGISTRO
+        tvRegister.setOnClickListener {
+            Toast.makeText(this, "Ir a pantalla de registro", Toast.LENGTH_SHORT).show()
+
+            // val intent = Intent(this, RegisterActivity::class.java)
+            // startActivity(intent)
         }
     }
 }
